3af4a02603175f47b9ee4c3160b308213fd466cf
Elfego Juarez <elfegoadair@gmail.com>
2022-03-20 19:13:48 -0600

    Boton para dibujar lineas

--- dibujo.js
+++ dibujo.js
@@ -1,25 +1,11 @@
-//ElfegoAdairJuarezArias
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#23e63a";//#23e63a
-var colort = "#ff3de5";//#ff3de5
 
-for (l=0; l < lineas; l++)
-{
-    yi = 10 * l;
-    xf = 10 * (l +1);
-    dibujarLinea(colorcito, 0, yi, xf, 300);
-    dibujarLinea(colort, 300, xf, yi, 0);
-    console.log("Linea " + l);
-}
-
-dibujarLinea(colorcito, 1, 1, 1, 299);
-dibujarLinea(colorcito, 1, 299, 299, 299);
-dibujarLinea(colort, 299, 1, 1, 1);
-dibujarLinea(colort, 299, 299, 299, 1);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -29,4 +15,28 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.lineTo(xfinal, yfinal);//En donde termina el Dibujo
   lienzo.stroke();//cerramos linea
   lienzo.closePath();//cerrar camino
+}
+
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "#23e63a";//#23e63a
+  var colort = "#ff3de5";//#ff3de5
+  var espacio = ancho / lineas;
+
+  for (l=0; l < lineas; l++)
+  {
+      yi = espacio * l;
+      xf = espacio * (l +1);
+      dibujarLinea(colorcito, 0, yi, xf, 300);
+      dibujarLinea(colort, 300, xf, yi, 0);
+      console.log("Linea " + l);
+  }
+
+  dibujarLinea(colorcito, 1, 1, 1, 299);
+  dibujarLinea(colorcito, 1, 299, 299, 299);
+  dibujarLinea(colort, 299, 1, 1, 1);
+  dibujarLinea(colort, 299, 299, 299, 1);
 }
